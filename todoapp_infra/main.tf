7ad7843c3db905624148c667fb33139cdb3a6053
shashank1-gupta <151348653+shashank1-gupta@users.noreply.github.com>
2026-05-13 13:36:39 +0530

    Added new rg in canada central - kindly review (#2)
    * ek choti si story add kr di
    
    * added new rg in canada central

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -4,10 +4,18 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+
+
+module "resource_group3" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "todoapp-rg3"
+  resource_group_location = "canadacentral"
+
 module "resource_group1" {
   source                  = "../modules/azurerm_resource_group"
   resource_group_name     = "todoapp-rg1"
   resource_group_location = "centralindia"
+
 }
 
 module "virtual_network" {
